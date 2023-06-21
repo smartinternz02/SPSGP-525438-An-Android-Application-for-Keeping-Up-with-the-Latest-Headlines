0e9bc8418fec7931bbc3e119dff2c9ceafdb1997
bharath9812 <mandadhibharathsimham@gmail.com>
2023-06-21 12:39:49 +0530

    commit by bharath

---
+++ app/src/main/java/com/example/headlines/Model.kt
@@ -0,0 +1,7 @@
+package com.example.headlines
+
+
+data class Movie(val name: String,
+                 val imageUrl: String,
+                 val desc: String,
+                 val category: String)
