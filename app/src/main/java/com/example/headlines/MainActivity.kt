7c02269e869b11c7a25c4e35f69c503477638247
bharath9812 <mandadhibharathsimham@gmail.com>
2023-06-21 12:04:08 +0530

    commit 1 by bharath

--- app/src/main/java/com/example/headlines/MainActivity.kt
+++ app/src/main/java/com/example/headlines/MainActivity.kt
@@ -18,6 +18,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             HeadlinesTheme {
                 // A surface container using the 'background' color from the theme
+                //bharath
+
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
