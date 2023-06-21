487f1a5aa821df351d342e9b01b01710d63fbf53
KVC7121 <kodichetu@gmail.com>
2023-06-21 12:19:29 +0530

    second commit

--- app/src/main/java/com/example/headlines/MainActivity.kt
+++ app/src/main/java/com/example/headlines/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HeadlinesTheme {
                 //Hi I am Nikhil
-                //chaitanya
+                //chaitanya , robo 2.0
                 //second change
                 // A surface container using the 'background' color from the theme
                 Surface(
